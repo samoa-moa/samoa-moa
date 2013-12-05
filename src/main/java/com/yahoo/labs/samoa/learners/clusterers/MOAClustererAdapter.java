9d469d65322c3a1f434d3947b9106582040cb4ef
Gianmarco De Francisci Morales <gdfm+github@gdfm.me>
2013-12-05 17:02:34 +0100

    Fixed license, compilation, dependencies

--- src/main/java/com/yahoo/labs/samoa/learners/clusterers/MOAClustererAdapter.java
+++ src/main/java/com/yahoo/labs/samoa/learners/clusterers/MOAClustererAdapter.java
@@ -2,26 +2,26 @@ package com.yahoo.labs.samoa.learners.clusterers;
 
 /*
  * #%L
- * SAMOA
+ * SAMOA-MOA
  * %%
- * Copyright (C) 2013 Yahoo! Inc.
+ * Copyright (C) 2013 SAMOA-MOA
  * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the 
+ * License, or (at your option) any later version.
  * 
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * You should have received a copy of the GNU General Public 
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
  * #L%
  */
-/**
- * License
- */
+
 import com.github.javacliparser.ClassOption;
 import com.github.javacliparser.Configurable;
 import com.yahoo.labs.samoa.instances.Instance;
@@ -33,9 +33,7 @@ import java.util.Random;
 import moa.clusterers.clustream.Clustream;
 
 /**
- *
  * Base class for moa classifiers.
- *
  */
 public class MOAClustererAdapter implements LocalClustererAdapter, Configurable {
 
